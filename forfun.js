5356cc98149e12e9320f9778d1639cae00fcc890
sara <saraslemanalhamede@gmail.com>
2024-02-15 22:25:45 +0200

    uptade

--- forfun.js
+++ forfun.js
@@ -2,11 +2,11 @@
 function dontclick() {
     document.getElementById("me").innerText = "I said don't Click";
     var img = document.getElementById("pic1");
-    img.src = 'images/dontClick.jpg'
+    img.src = 'images/dontClick.jpg';
 
 }
 function dontclick1() {
     document.getElementById("me1").innerText = "I said don't Click";
     var img = document.getElementById("pic2");
-    img.src = 'images/dontClick.jpg'
+    img.src = 'images/dontClick.jpg';
 }
